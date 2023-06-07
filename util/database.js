587d4a30d82ee39290e0cba44f2a687f8882c594
NBahrom <135149522+NBahrom@users.noreply.github.com>
2023-06-07 13:53:21 +0500

    Update database.js

--- util/database.js
+++ util/database.js
@@ -1,13 +1,8 @@
 const Sequelize = require('sequelize')
 
-const sequelize = new Sequelize('MLP', 'root', 'Baxa9229', {
+const sequelize = new Sequelize('', '', '', {
     dialect: 'mysql',
     host: 'localhost'
 })
-
-// const sequelize = new Sequelize('farruhon_mlp', 'farruhon_mlp_user', 'mlp_user_9229', {
-//     dialect: 'mysql',
-//     host: 'localhost'
-// })
 
 module.exports = sequelize
